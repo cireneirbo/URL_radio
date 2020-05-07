2d49f7c4e35ac386112bf216a82a052667a320a5
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-05-06 21:55:43 -0400

    added some features, but nothing works

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,18 @@
-document.getElementById('btn').addEventListener('mouseover', function() {
-    console.log("hey there world");
+document.getElementById('track-caption').addEventListener('mouseover', function() {
+    this.innerText = "Reset the browser!";
 });
+
+
+/*
+the plan!
+
+have an array that holds urls and uses a switch function to randomly choose one to play in an audio tab.
+allow users to add to the array of music with a form that pushes content into the array
+
+allow users to create an account
+possibly allows them to vote on the next song
+possibly add a chat room or that chat room you know about
+
+
+*/
+
